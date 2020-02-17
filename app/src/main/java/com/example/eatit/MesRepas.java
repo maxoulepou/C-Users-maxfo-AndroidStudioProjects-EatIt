a4777f97ef2bac877134348c4c4ccf65a4d1f2ba
Julie <Nguyen>
2020-02-17 09:14:08 +0100

    interface
    bienvenue, connexion, création de compte, mes repas (+ création classe), mes objectifs (+création classe)

---
+++ app/src/main/java/com/example/eatit/MesRepas.java
@@ -0,0 +1,31 @@
+package com.example.eatit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class MesRepas extends AppCompatActivity {
+
+    private EditText jourRepas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mes_repas);
+
+        jourRepas=(EditText)findViewById(R.id.jourRepas);
+        jourRepas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Calendar cal= Calendar.getInstance();
+               int year = cal.get(Calendar.YEAR);
+               int month= cal.get(Calendar.MONTH);
+               int day= cal.get(Calendar.DAY_OF_MONTH)
+            }
+        });
+    }
+}
