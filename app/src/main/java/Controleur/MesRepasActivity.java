51499877ba1673d51985f8d532fffaaa0176ca67
Foucher Maxime <maxfoucher96@gmail.com>
2020-02-18 16:59:18 +0100

    Take photo
    enregistrer photo et activityTakePhoto

--- app/src/main/java/Controleur/MesRepasActivity.java
+++ app/src/main/java/Controleur/MesRepasActivity.java
@@ -1,14 +1,86 @@
 package Controleur;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import com.example.eatit.R;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MesRepasActivity extends AppCompatActivity {
+
+    private String currentPhotoPath;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mes_repas);
     }
 
+    //fonction pour appeller un intent pour prendre une photo
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Ensure that there is a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+            //Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch(IOException ex){
+                //Ettor occurred while creating the file
+            }
+            //Continue only if the File was successfully created
+            if(photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    //Récupérer l'image en icone/miniature
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data"); //Good pour un icone, peut être que nous devrioons modifier tout cela si nos photos nécessitents d'être plus grandes
+//            ImageView.setImageBitmap(imageBitmap);
+//        }
+//    }
+
+    //Creation du fichier de sauvegarde pour enregistrer l'image en full size
+    private File createImageFile() throws IOException{
+        //Create an image file name using date
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,      //Prefix
+                ".jpeg",     //sufffix
+                storageDir         //directory
+        );
+
+        //save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 }
+
+
+//méthode pour vérifier si un appareil photo est fonctionnel
+//hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)
+
