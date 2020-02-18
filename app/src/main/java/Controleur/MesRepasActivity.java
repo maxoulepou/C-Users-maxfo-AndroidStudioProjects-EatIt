d71fc7986e461539b050b3b22032137ecfd2b072
Foucher Maxime <maxfoucher96@gmail.com>
2020-02-18 15:31:09 +0100

    Gérer profil
    Méthode de suppression et déconnexion

---
+++ app/src/main/java/Controleur/MesRepasActivity.java
@@ -0,0 +1,14 @@
+package Controleur;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.eatit.R;
+
+public class MesRepasActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mes_repas);
+    }
+
+}
---
+++ app/src/main/java/Model/BaseActivity.java
@@ -0,0 +1,14 @@
+package Model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected FirebaseUser getCurrentUser(){return FirebaseAuth.getInstance().getCurrentUser(); }
+
+    protected Boolean isCurrentUserLogged(){ return (this.getCurrentUser() != null); }
+
+}
