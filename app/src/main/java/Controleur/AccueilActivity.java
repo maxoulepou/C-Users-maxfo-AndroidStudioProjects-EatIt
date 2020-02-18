2e7dc906d41707f4d3859c3d29571e8000902fee
Foucher Maxime <maxfoucher96@gmail.com>
2020-02-18 11:02:57 +0100

    Correction erreur
    erreur suite au merge

--- app/src/main/java/Controleur/AccueilActivity.java
+++ app/src/main/java/Controleur/AccueilActivity.java
@@ -1,28 +1,4 @@
 package Controleur;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import com.example.eatit.R;
-import android.widget.TextView;
-
-public class AccueuilActivity extends AppCompatActivity {
-
-    private TextView infoUtilisateur;
-   // private DataBase bd;
-
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accueil);
-
-        infoUtilisateur = (TextView) findViewById(R.id.infoUtilisateurs);
-/**     bd = new DataBase(this);
-
-        bd.nouveauProfil("Anne","Onymous","patient","annie","mdp");
-        bd.nouveauProfil("Pierre","Caillou","Dieteticien","pierro","mdp");
-
-        bd.close();
- **/
-    }
+public class AccueilActivity {
 }
