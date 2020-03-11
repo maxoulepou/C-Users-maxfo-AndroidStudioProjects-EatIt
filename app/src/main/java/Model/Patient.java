b9c63d368d2df29d22fa791eb81cb41681796c8c
Foucher Maxime <maxfoucher96@gmail.com>
2020-03-11 13:14:03 +0100

    Connexion
    Création compte et connexion avec sqLite

--- app/src/main/java/Model/Patient.java
+++ app/src/main/java/Model/Patient.java
@@ -8,10 +8,11 @@ public class Patient {
     private long id;
     private String nom;
     private String prenom;
-    private Date dateNaissance;
+    private String dateNaissance;
     private String sexe; // restriction homme femme à faire
     private String email;
     private String adresse, ville, contactUrgence;
+    private String mdp;
     private int codePostal;
     private ArrayList<Repas> lCarnetRepas;
     private ArrayList<Intervention> lIntervention;
@@ -21,13 +22,23 @@ public class Patient {
     private ArrayList<Poids> lPoids;
 
 
-    public Patient(long id, String nom, String prenom, String sexe, Date date){ //peut etre ajouter des parametres, a voir lors de la creation du controlleur
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public Patient(String email, String mdp, String nom, String prenom, String sexe, String dateN){ //peut etre ajouter des parametres, a voir lors de la creation du controlleur
         super();
-        this.id = id;
+       // this.id = id;
+        this.email = email;
+        this.mdp = mdp;
         this.nom = nom;
         this.prenom = prenom;
         this.sexe = sexe;
-        this.dateNaissance = date;
+        this.dateNaissance = dateN;
     }
 
     public void setEmail(String email) {
@@ -78,11 +89,11 @@ public class Patient {
         return sexe;
     }
 
-    public Date getDateNaissance() {
+    public String getDateNaissance() {
         return dateNaissance;
     }
 
-    public void setDateNaissance(Date dateNaissance) {
+    public void setDateNaissance(String dateNaissance) {
         this.dateNaissance = dateNaissance;
     }
 
